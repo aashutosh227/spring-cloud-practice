154b5fa385a3ca39b034f40b37cf01704492ff08
Aashutosh <aashutoshsingh126@gmail.com>
2026-03-10 21:52:25 +0530

    Every call should go via gateway . Hence integrating gateway in feign client instead of coupons ms

--- ms-product/src/main/java/com/aashutosh/ms_product/feignclients/CouponClient.java
+++ ms-product/src/main/java/com/aashutosh/ms_product/feignclients/CouponClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("MS-COUPONS")
+@FeignClient("MS-GATEWAY")
 public interface CouponClient {
 
     @GetMapping("/couponApi/coupons/{code}")
